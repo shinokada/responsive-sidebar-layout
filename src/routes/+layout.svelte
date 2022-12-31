f47bde624296abb19856fc1221fbe3306cd5ee3e
Shinichi Okada <okada.shin@gmail.com>
2022-12-31 09:09:56 +0900

    add sidebar to drawer component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,14 +18,53 @@
   } from 'flowbite-svelte';
   import { Cog } from 'svelte-heros-v2';
   import { sineIn } from 'svelte/easing';
+
+  // Navigation component
   let drawerHidden: boolean = false;
   const toggleDrawer = () => {
     drawerHidden = false;
   };
-
   let divClass = 'w-full md:block md:w-auto pr-8';
   let ulClass = 'flex flex-col p-4 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-lg md:font-medium';
+
+  // Drawer component
+  let backdrop: boolean = false;
+  let transitionParams = {
+    x: -320,
+    duration: 200,
+    easing: sineIn
+  };
+  let activateClickOutside = true;
+  $: if (width >= breakPoint) {
+    drawerHidden = false;
+    activateClickOutside = false;
+  } else {
+    drawerHidden = true;
+    activateClickOutside = true;
+  }
+  let breakPoint: number = 1024;
+  let width: number;
+  onMount(() => {
+    if (width >= breakPoint) {
+      drawerHidden = false;
+      activateClickOutside = false;
+    } else {
+      drawerHidden = true;
+      activateClickOutside = true;
+    }
+  });
+
+  // Sidebar component
+  const toggleSide = () => {
+    if (width < breakPoint) {
+      drawerHidden = !drawerHidden;
+    }
+  };
+  $: activeUrl = $page.url.pathname;
+  let spanClass = 'pl-2 self-center text-md text-gray-900 whitespace-nowrap dark:text-white';
 </script>
+
+<svelte:window bind:innerWidth={width} />
 
 <Navbar let:hidden let:toggle>
   <NavHamburger on:click={toggleDrawer} btnClass="ml-3 lg:hidden" />
@@ -44,7 +83,51 @@
     >
   </NavUl>
 </Navbar>
-
+<!--  -->
+<Drawer
+  transitionType="fly"
+  {backdrop}
+  {transitionParams}
+  bind:hidden={drawerHidden}
+  bind:activateClickOutside
+  width="w-64"
+  class="overflow-scroll pb-32"
+  id="sidebar"
+>
+  <div class="flex items-center">
+    <CloseButton on:click={() => (drawerHidden = true)} class="mb-4 dark:text-white lg:hidden" />
+  </div>
+  <Sidebar asideClass="w-54">
+    <SidebarWrapper divClass="overflow-y-auto py-4 px-3 rounded dark:bg-gray-800">
+      <SidebarGroup>
+        <SidebarItem label="Home" href="/" on:click={toggleSide} active={activeUrl === `/`} />
+        <SidebarItem
+          label="About"
+          href="/pages/about"
+          {spanClass}
+          on:click={toggleSide}
+          active={activeUrl === '/pages/about'}
+        />
+        <SidebarDropdownWrapper label="Articles">
+          <SidebarItem
+            label="Article 1"
+            href={`/blog/2023-01-01`}
+            {spanClass}
+            on:click={toggleSide}
+            active={activeUrl === '/blog/2023-01-01'}
+          />
+          <SidebarItem
+            label="Article 2"
+            href="/blog/2023-01-02"
+            {spanClass}
+            on:click={toggleSide}
+            active={activeUrl === '/blog/2023-01-02'}
+          />
+        </SidebarDropdownWrapper>
+      </SidebarGroup>
+    </SidebarWrapper>
+  </Sidebar>
+</Drawer>
 <div class="flex px-4 mx-auto w-full">
   <main class="lg:ml-72 w-full mx-auto">
     <slot />
